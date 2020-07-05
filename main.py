e84e7795d48f92b9c02f66a9797f4ff9ee96ca25
julgrahn <alexander.grahn@hotmail.se>
2020-07-06 01:54:29 +0200

    gridlayout, initialize classes needed, run app

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('design.kv')
 
+class LoginScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
 
