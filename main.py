2924d81fe0c21c8aace16c9a60fac5dd27606764
julgrahn <alexander.grahn@hotmail.se>
2020-07-06 03:50:38 +0200

    image button for logout with state change on hover

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json, glob
 from datetime import datetime
 from pathlib import Path
 import random
+from hoverable import HoverBehavior
 
 
 Builder.load_file('design.kv')
@@ -58,6 +61,8 @@ class LoginScreenSuccess(Screen):
         else:
             self.ids.quote.text = "Try another feeling"
 
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class MainApp(App):
     def build(self):
